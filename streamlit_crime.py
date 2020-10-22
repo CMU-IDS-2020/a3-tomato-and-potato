e089fd860bacacbaa93cf0a17e2ac71857ab6dab
greyring <greyring@163.com>
2020-10-22 00:08:51 -0400

    add more graph

--- streamlit_crime.py
+++ streamlit_crime.py
@@ -8,8 +8,8 @@ st.title("Chicago Crimes")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    crimes_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-tomato-and-potato/master/Crimes_2019.csv"
-    return pd.read_csv(crimes_url).sample(n=1000)
+    crimes_url = "https://raw.githubusercontent.com/CMU-IDS-2020/a3-tomato-and-potato/master/Crimes_2019_Jan.csv"
+    return pd.read_csv(crimes_url)
 
 @st.cache
 def load_map():
@@ -25,11 +25,11 @@ if st.checkbox('Show Raw Data'):
 picked = alt.selection_multi(fields = ['Primary Type'])
 
 type_chart = alt.Chart(df).mark_bar().encode(
-    alt.X('Primary Type:N', sort='-y'),
-    alt.Y('count():Q'),
+    alt.Y('Primary Type:N', sort='-x'),
+    alt.X('count():Q'),
     color = alt.condition(picked, alt.value('#1f77b4'), alt.value('lightgray')),
     tooltip = ['Primary Type', 'count()']
-).properties(height=600).add_selection(picked)
+).add_selection(picked)
 
 chicago_map = \
 alt.Chart(df).transform_filter(picked).transform_aggregate(
@@ -49,12 +49,33 @@ alt.Chart(df).transform_filter(picked).transform_aggregate(
     as_=['community']
 )
 
+arrest_map = \
+alt.Chart(df).transform_filter(picked).transform_aggregate(
+    case_count='count()',
+    arrest_count='sum(Arrest)',
+    groupby=['Community Area']
+).transform_calculate(
+    arrest_rate='datum.arrest_count / datum.case_count'
+).transform_lookup(
+    lookup='Community Area',
+    from_=alt.LookupData(geojson, 'properties.area_numbe', ['properties.community', 'type', 'geometry']),
+).mark_geoshape(
+    stroke='white'
+).encode(
+    color='arrest_rate:Q',
+    tooltip=['community:N', 'arrest_rate:Q']
+).transform_lookup(
+    lookup='Community Area',
+    from_=alt.LookupData(geojson, 'properties.area_numbe', ['properties.community']),
+    as_=['community']
+)
+
 brush = alt.selection_interval(encodings=['x'])
 
 time_chart = \
 alt.Chart(df).transform_filter(picked)\
 .mark_area().encode(
-    alt.X('yearmonth(Date)'),
+    alt.X('monthdate(Date)'),
     alt.Y('count()'),
 ).properties(width=600)
 
@@ -82,8 +103,9 @@ st.write(
         # ),
         type_chart,
         chicago_map,
+        arrest_map,
         time_chart.encode(
-            alt.X('yearmonthdate(Date)', scale=alt.Scale(domain=brush))
+            alt.X('monthdate(Date)', scale=alt.Scale(domain=brush))
         ),
         alt.layer(
             time_chart.add_selection(brush).encode(
@@ -95,3 +117,50 @@ st.write(
     ).resolve_scale(color='independent')
 )
 
+picked = alt.selection_multi(fields=['Community Area'])
+
+chicago_map = \
+alt.Chart(df).transform_aggregate(
+    case_count='count()',
+    groupby=['Community Area']
+).transform_lookup(
+    lookup='Community Area',
+    from_=alt.LookupData(geojson, 'properties.area_numbe', ['properties.community', 'type', 'geometry']),
+).mark_geoshape(
+    stroke='white'
+).encode(
+    color=alt.condition(picked, 'case_count:Q', alt.value('lightgray')),
+    tooltip=['community:N', 'case_count:Q']
+).transform_lookup(
+    lookup='Community Area',
+    from_=alt.LookupData(geojson, 'properties.area_numbe', ['properties.community']),
+    as_=['community']
+).add_selection(picked)
+
+type_chart = \
+alt.Chart(df)\
+.mark_bar().encode(
+    alt.Y('Primary Type:N', sort='-x'),
+    alt.X('count():Q'),
+    tooltip = ['Primary Type', 'count()']
+)
+
+heatmap = \
+alt.Chart(df).transform_filter(picked)\
+.mark_rect().encode(
+    alt.X('hours(Date):O', title='hour of day'),
+    alt.Y('day(Date):O', title='date'),
+    alt.Color('count()', title='Case Count'),
+    tooltip=['hours(Date)', 'day(Date)', 'count()']
+)
+
+st.write(
+    alt.vconcat(
+        chicago_map,
+        alt.layer(
+            type_chart.encode(color = alt.value('lightgray')),
+            type_chart.transform_filter(picked)
+        ),
+        heatmap
+    ).resolve_scale(color='independent')
+)
